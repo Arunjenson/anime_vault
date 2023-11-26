b2c7aec4c17b8ed38e24e9bcf0ca1e7b86b6a37c
Arunjenson-SurveySparrow <arun.jenson@surveysparrow.com>
2023-11-26 13:46:09 +0530

    implemented the infinite scrolling behavior and added the server action to get the data

---
+++ app/action.ts
@@ -0,0 +1,7 @@
+"use server"
+
+export const getData = async (page: number) => {
+    const res = await fetch(`https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`);
+    const data = await res.json();
+    return data
+}
